cd9c2fb08cf0e7b47bd3d26fc69bbaa2ba5bc45d
AlexisBoivin <2165774@carrefour.cegepvicto.ca>
2025-05-05 14:49:27 -0400

    5 mai 1.5

--- Assets/Scripts/ControleurFusils.cs
+++ Assets/Scripts/ControleurFusils.cs
@@ -107,63 +107,150 @@
 //        yield return new WaitForSeconds(1f);
 //        enAttaque = false;
 //    }
-}
+//}
+//using Oculus.Interaction;
+//using Oculus.Interaction.HandGrab;
+//using UnityEngine;
+//using System.Collections;
+
+//public class ControlleurFusil : MonoBehaviour
+//{
+//    [SerializeField] 
+//    private Transform canon;
+//    [SerializeField]
+//    private GameObject prefabBalle;
+//    [SerializeField] 
+//    private float vitesseBalle = 20f;
+//    [SerializeField] 
+//    private GrabInteractable grabInteractable;
+
+//    //private bool enAttaque = false;
+//    void Update()
+//    {
+//        if (grabInteractable == null || grabInteractable.SelectingInteractors.Count == 0)
+//            return;
+
+//        foreach (var interactor in grabInteractable.Interactors)
+//        {
+//            string interactorName = interactor.name.ToLower();
+
+//            if (interactorName.Contains("right") && OVRInput.Get(OVRInput.RawButton.RIndexTrigger))
+//            {
+//                onTirer();
+//                break;
+//            }
+//            else if (interactorName.Contains("left") && OVRInput.Get(OVRInput.RawButton.LIndexTrigger))
+//            {
+//                onTirer();
+//                break;
+//            }
+//        }
+
+//    }
+
+
+//    public void onTirer()
+//    {
+//        //enAttaque = true;
+//        GameObject balleExistante = Instantiate(prefabBalle, canon.position, canon.rotation);
+//        balleExistante.GetComponent<Rigidbody>().AddForce(canon.forward * vitesseBalle, ForceMode.Impulse);
+
+//        Destroy(balleExistante, 10f);
+//        //yield return new WaitForSeconds(1f);
+//        //enAttaque = false;
+//        //StartCoroutine(EffetTir());
+//    }
+
+//    //private IEnumerator EffetTir()
+//    //{
+//    //    GameObject balleExistante = Instantiate(prefabBalle, canon.position, canon.rotation);
+//    //    balleExistante.GetComponent<Rigidbody>().AddForce(canon.forward * vitesseBalle, ForceMode.Impulse);
+
+//    //    Destroy(balleExistante, 10f);
+//    //    yield return new WaitForSeconds(1f);
+//    //    enAttaque = false;
+//    //}
+//}
 using Oculus.Interaction;
 using Oculus.Interaction.HandGrab;
+using System;
+using System.Drawing;
+using Unity.Burst.Intrinsics;
+using Unity.VisualScripting;
 using UnityEngine;
 using System.Collections;
 
 public class ControlleurFusil : MonoBehaviour
 {
-    [SerializeField] private Transform canon;
-    [SerializeField] private GameObject prefabBalle;
-    [SerializeField] private float vitesseBalle = 20f;
-    [SerializeField] private GrabInteractable grabInteractable;
+    /// <summary>
+    /// Emplacement du canon
+    /// </summary>
+    [SerializeField]
+    private Transform canon;
 
+    /// <summary>
+    /// Prefab d'une balle
+    /// </summary>
+    [SerializeField]
+    private GameObject prefabBalle;
+
+
+    /// <summary>
+    /// Vitesse de la balle
+    /// </summary>
+    [SerializeField]
+    private float vitesseBalle = 20f;
+
+    /// <summary>
+    /// Booléen qui représente si le joueur est entrain de tirer. Sers à mettre un délai entre les balles.
+    /// </summary>
     private bool enAttaque;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Update()
     {
-        if (grabInteractable == null || grabInteractable.SelectingInteractors.Count == 0 || enAttaque)
-            return;
+  
 
-        foreach (var interactor in grabInteractable.Interactors)
+        // Déterminer la main via l'interactor
+        if (OVRInput.Get(OVRInput.RawButton.RIndexTrigger))
         {
-            string interactorName = interactor.name.ToLower();
-
-            if (interactorName.Contains("right") && OVRInput.Get(OVRInput.RawButton.RIndexTrigger))
-            {
-                onTirer();
-                break;
-            }
-            else if (interactorName.Contains("left") && OVRInput.Get(OVRInput.RawButton.LIndexTrigger))
-            {
-                onTirer();
-                break;
-            }
+            onTirer();
         }
-
+        //mainQuiTient.gameObject.name.ToLower().Contains("left")
+        else if (OVRInput.Get(OVRInput.RawButton.LIndexTrigger))
+        {
+            onTirer();
+        }
     }
 
-
+    /// <summary>
+    /// Tirer une balle
+    /// </summary>
     public void onTirer()
     {
         enAttaque = true;
-        GameObject balleExistante = Instantiate(prefabBalle, canon.position, canon.rotation);
-        balleExistante.GetComponent<Rigidbody>().AddForce(canon.forward * vitesseBalle, ForceMode.Impulse);
-
-        Destroy(balleExistante, 10f);
-        //yield return new WaitForSeconds(1f);
-        enAttaque = false;
-        //StartCoroutine(EffetTir());
+        AudioSource sourceaudio = GetComponent<AudioSource>();
+        sourceaudio.Play();
+        StartCoroutine(EffetTir());
     }
 
-    //private IEnumerator EffetTir()
-    //{
-    //    GameObject balleExistante = Instantiate(prefabBalle, canon.position, canon.rotation);
-    //    balleExistante.GetComponent<Rigidbody>().AddForce(canon.forward * vitesseBalle, ForceMode.Impulse);
+    /// <summary>
+    /// Coroutine pour tirer une balle
+    /// </summary>
+    /// <returns>Un délai de une seconde entre chaque tir.</returns>
+    private IEnumerator EffetTir()
+    {
+        //Créer la balle
+        GameObject balleExistante = Instantiate(prefabBalle);
+        balleExistante.transform.position = canon.position;
+        balleExistante.transform.rotation = canon.rotation;
+        //vitesse et direction de la balle
+        balleExistante.GetComponent<Rigidbody>().AddForce(canon.forward * vitesseBalle, ForceMode.Impulse);
 
-    //    Destroy(balleExistante, 10f);
-    //    yield return new WaitForSeconds(1f);
-    //    enAttaque = false;
-    //}
+        //La détruire après 10 secondes
+        Destroy(balleExistante, 10);
+        yield return new WaitForSeconds(1f);
+        enAttaque = false;
+    }
 }
